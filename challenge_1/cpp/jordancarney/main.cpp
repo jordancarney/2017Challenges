2b6eb0583303ed5e50ff5892dac38a03783d65e0
Jordan <jordan@jordan-carney.com>
2017-01-03 22:35:59 -0500

    Add c++ solution to challenge 1

---
+++ challenge_1/cpp/jordancarney/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main(int argc, char* argv[]) {
+  std::string input;
+  std::getline(std::cin,input);
+  std::reverse(input.begin(), input.end());
+  std::cout << input << std::endl;
+  return 0;
+}
